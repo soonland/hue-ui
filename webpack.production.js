8ac52c6ed221502e2319a98b128a5d22686b8542
soonland <76574139+soonland@users.noreply.github.com>
2022-02-02 20:48:54 -0500

    fix: fixed webpack.production

--- webpack.production.js
+++ webpack.production.js
@@ -2,7 +2,7 @@
 /* eslint object-shorthand: 0 */
 /* eslint func-names: 0 */
 /* eslint global-require: 0 */
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common');
 
